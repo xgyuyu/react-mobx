6e02d32d99d9d8cbf3e716e43a3f7f7e4a2659ce
mac <mac@macdeMacBook-Pro.local>
2019-03-25 16:15:20 +0800

    完成基本，可以跑通了。后期慢慢搭建框架

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './src/css/index.less'
+import './src/css/index.css'
+import App from './src/App'
+
+
+ReactDOM.render(
+  <App/>, document.getElementById('app')
+)
